d24961869d39bb513c4e7681f30fc7c6528127e3
Sinthujan Sivanantham <ssinthujan20@icloud.com>
2025-12-19 10:27:47 +0100

    Add vercel.json rewrite rule

--- vite.config.js
+++ vite.config.js
@@ -5,29 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    // 1. Warn-Limit erhöhen (z.B. auf 1600 kBs statt 500 kBs)
-    chunkSizeWarningLimit: 1600,
-
-    // 2. Intelligentes Aufteilen der Dateien (Code Splitting)
-    // Das sorgt dafür, dass Three.js in eine eigene Datei gepackt wird, 
-    // die der Browser cachen kann.
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Packt Three.js und React-Three-Fiber in eine separate Datei 'vendor_three'
-          if (id.includes('node_modules')) {
-            if (id.includes('three') || id.includes('@react-three')) {
-              return 'vendor_three';
-            }
-            if (id.includes('gsap')) {
-              return 'vendor_gsap';
-            }
-            if (id.includes('@supabase')) {
-              return 'vendor_supabase';
-            }
-          }
-        },
-      },
-    },
-  },
+    chunkSizeWarningLimit: 3000, // Damit die Warnung wegbleibt
+  }
 })
